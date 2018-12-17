3f9d0d843c0728b91ff44007381d3ae0cd0f9ee6
Liu Chaoyang <chaoyanglius@gmail.com>
2018-12-16 19:42:30 -0500

    add script of print pg num per osd

---
+++ osd_tools/print_pgnum_by_osd.sh
@@ -0,0 +1,19 @@
+# print osd status 
+echo "OSD STATUS"
+ceph osd stat
+ceph pg dump | awk '
+BEGIN { IGNORECASE = 1 }
+ /^PG_STAT/ { col=1; while($col!="UP") {col++}; col++ }
+ /^[0-9a-f]+\.[0-9a-f]+/ { match($0,/^[0-9a-f]+/); pool=substr($0, RSTART, RLENGTH); poollist[pool]=0;
+ up=$col; i=0; RSTART=0; RLENGTH=0; delete osds; while(match(up,/[0-9]+/)>0) { osds[++i]=substr(up,RSTART,RLENGTH); up = substr(up, RSTART+RLENGTH) }
+ for(i in osds) {array[osds[i],pool]++; osdlist[osds[i]];}
+}
+END {
+ printf("\n");
+ printf("pool :\t"); for (i in poollist) printf("%s\t",i); printf("| SUM \n");
+ for (i in poollist) printf("--------"); printf("----------------\n");
+ for (i in osdlist) { printf("osd.%i\t", i); sum=0;
+   for (j in poollist) { printf("%i\t", array[i,j]); sum+=array[i,j]; sumpool[j]+=array[i,j] }; printf("| %i\n",sum) }
+ for (i in poollist) printf("--------"); printf("----------------\n");
+ printf("SUM :\t"); for (i in poollist) printf("%s\t",sumpool[i]); printf("|\n");
+}'
