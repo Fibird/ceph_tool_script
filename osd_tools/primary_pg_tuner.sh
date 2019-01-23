da4c349e25a124e576dc80b69c2341c56a8ca76d
Liu Chaoyang <chaoyanglius@gmail.com>
2019-01-23 04:27:14 -0500

    make primary pg tuner to adjust osds

--- osd_tools/primary_pg_tuner.sh
+++ osd_tools/primary_pg_tuner.sh
@@ -2,7 +2,7 @@
 
 # --------------------------------------------------------
 # Shell script to tune primary pg number by osd
-# Copyright(c)2019 Liu Chaoyang (chaoyanglius@gmail.com) 
+# Copyright(c)2019 Liu Chaoyang (chaoyanglius@gmail.com)
 # --------------------------------------------------------
 
 # Parameters
@@ -12,15 +12,14 @@
 
 # Get primary pg number
 
-if [[ $# -ne 2 ]]
+if [[ $# -ne 1 ]]
 then
   echo "Please specify pool id and iteration times!"
   exit 1
 fi
 
-pool_id=$1
 set_dev=2
-iter_times=$2
+iter_times=$1
 
 # get osd list
 osd_list=($(ceph osd ls))
@@ -45,63 +44,17 @@ stride=0.01
 # get primary pg numbers by osd of the pool
 get_pri_pgs()
 {
-	pri_pgs_by_osd=($(ceph pg dump | awk -v pool_id="$pool_id" '
-BEGIN {
-   IGNORECASE = 1 
-}
-# get column of primary pg num
-/^PG_STAT/ { 
-  prim_col=1;
-  while($prim_col!="UP_PRIMARY") {
-    prim_col++
-  }
-  prim_col++;
-}
-
-/^[0-9a-f]+\.[0-9a-f]+/ { 
-  match($0,/^[0-9a-f]+/); 
-  pool=substr($0,RSTART,RLENGTH); 
-  poollist[pool]=0;
-  up_primary=$prim_col;
-  j=0;
-  RSTART=0; 
-  RLENGTH=0; 
-  delete prim_osds;
-
-  # primary pg
-  while(match(up_primary,/[0-9]+/)>0) {
-    prim_osds[++j]=substr(up_primary,RSTART,RLENGTH);
-    up_primary=substr(up_primary,RSTART+RLENGTH);
-  }
-
-  for(j in prim_osds) {
-    prim_array[prim_osds[j],pool]++;
-    prim_osdlist[prim_osds[j]];
-  }
-}
-
-END {
-  sum=0; count=0;
-  # print primary pg num
-  for (i in prim_osdlist) { 
-	  sum+=prim_array[i,pool_id];
-	  count++;
-      print(prim_array[i,pool_id]); 
-  } 
-  print(sum);
-  for (i in prim_osdlist) {
-	  print(i);
-  }
-}
-'))
-
-	# echo ${pri_pgs_by_osd[@]}
+    pri_pgs_by_osd=($(ceph pg dump osds | awk '/^[0-9]+\s/ {print($10)}'))
+    by_osds=($(ceph pg dump osds | awk '/^[0-9]+\s/ {print($1)}'))
+	echo ${pri_pgs_by_osd[@]}
 	max=${pri_pgs_by_osd[0]}
 	min=${pri_pgs_by_osd[1]}
 
 	count=${#pri_pgs_by_osd[@]}
-	count=$[count/2]
-	sum=${pri_pgs_by_osd[$count]}
+    sum=0
+    for i in ${pri_pgs_by_osd[@]}; do
+        let sum+=i
+    done
 	average=$[sum/count]
 }
 
@@ -119,14 +72,14 @@ for (( j=0; j<$iter_times; j++ )); do
 		if [[ $min -gt $i ]]; then
 			min=$i
 		fi
-		tmp_aff=${osd_pri_aff[pri_pgs_by_osd[$[counter+count+1]]]}
+		tmp_aff=${osd_pri_aff[by_osds[$counter]]}
 		if [[ $i -lt $average && $(echo ${tmp_aff%.*}) -ne 1 ]]; then
 			tmp_aff=$(echo "ibase=10; scale=2; $tmp_aff+$stride" | bc)
 		fi
 		if [[ $i -gt $average ]]; then
 			tmp_aff=$(echo "ibase=10; scale=2; $tmp_aff-$stride" | bc)
 		fi
-		osd_pri_aff[pri_pgs_by_osd[$[counter+count+1]]]=$tmp_aff
+		osd_pri_aff[by_osds[$counter]]=$tmp_aff
 		let counter++
 		diff=$(echo "ibase=10; scale=2; $i-$average" | bc)
 		abs_diff=$(echo $diff | awk ' { if($1>=0) { print $1} else {print $1*-1 }}')
@@ -139,14 +92,14 @@ for (( j=0; j<$iter_times; j++ )); do
 	# adjust osd primary affinity
 	for i in ${osd_list[@]}; do
 		ceph osd primary-affinity $i ${osd_pri_aff[i]}
-		echo $i
+		# echo $i
 	done
 done
 
-echo "max:$max"
-echo "min:$min"
-echo "deviation:$deviation"
-echo "average:$average"
-echo "sum:$sum"
-#echo "count:$count"
+echo "max: $max"
+echo "min: $min"
+echo "deviation: $deviation"
+echo "average: $average"
+echo "sum: $sum"
+#echo "count: $count"
 #echo ${osd_pri_aff[@]}
