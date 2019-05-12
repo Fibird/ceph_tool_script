ab2d5ac755b19b9ac90453125a91d19d4d71b0be
root <chaoyanglius@gmail.com>
2019-05-12 01:48:55 -0400

    improve script of deploy on single node
    * add auto-get public network
    * add remove-old data commands

--- deploy/deploy_on_single_node.sh
+++ deploy/deploy_on_single_node.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+# Liu Chaoyang (chaoyanglius@gmail.com)
+# This script is used to deploy ceph on single node
+# **Note:** This script must be run by root
+# 2019-05-12 13:24
 
-if [[ $# -ne 1 ]]
-then
-  echo "Please specify public network!"
-  exit 1
-fi
-
-ip_seg=$1
-node_name=`hostname`
+public_network=$(ip -o -f inet addr show | awk '/scope global noprefixroute/ {print $4}')
+node_name=$(hostname)
 ipadd=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
 
 # backup hosts file
@@ -16,12 +14,11 @@ cp /etc/hosts /etc/hosts.$cur_time.bk
 # add node ip into hosts
 echo "$ipadd $node_name" >> /etc/hosts
 
-# copy keyring file
-ssh-keygen -t rsa
-ssh-copy-id root@$node_name
+# clean older dir
+rm -rf ~/ceph_cluster
 
-mkdir ~/ceph-cluster
-cd ~/ceph-cluster
+mkdir ~/ceph_cluster
+cd ~/ceph_cluster
 
 # install necessary software
 yum install tree nmap sysstat lrzsz dos2unix wegt git net-tools -y
@@ -42,11 +39,15 @@ yum makecache fast
 # install ceph-deploy
 yum install http://mirrors.163.com/ceph/rpm-mimic/el7/noarch/ceph-deploy-1.5.39-0.noarch.rpm
 
+# remove older ceph and ceph data
+ceph-deploy purge $node_name
+ceph-deploy purgedata $node_name
+
 # create ceph cluster
 ceph-deploy new $node_name
 
 # set public network
-echo "public_network=$ip_seg/24" >> ./ceph.conf
+echo "public_network=$public_network" >> ~/ceph_cluster/ceph.conf
 
 # install ceph components
 ceph-deploy install --release mimic --repo-url http://mirrors.163.com/ceph/rpm-mimic/el7 --gpg-url http://mirrors.163.com/ceph/keys/release.asc $node_name
